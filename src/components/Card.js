c42aa9f205e44fe184e89b56a0532e07f8c8e907
JenelMyers <jenelmyers@JenelMyerss-MacBook-Pro.local>
2018-03-05 20:16:53 -0700

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View } from 'react-native';
+
+const Card = () => {
+  return (
+    <View></View
+  );
+};
+
+const styles {
+  containerStyle:{
+    borderWidth: 1,
+    borderWidth: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+  }
+};
+
+export default Card
