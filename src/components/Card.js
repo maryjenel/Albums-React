440c707e25613dbf17d111ca91bfcb36d14d1cea
JenelMyers <jenelmyers@JenelMyerss-MacBook-Pro.local>
2018-03-05 21:01:34 -0700

    added cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import {View } from 'react-native';
+import { View } from 'react-native';
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <View></View
+    <View style={style.containerStyle}>
+    {props.children}
+    </View>
   );
 };
 
-const styles {
-  containerStyle:{
+const style = {
+  containerStyle: {
     borderWidth: 1,
     borderWidth: 2,
     borderColor: '#ddd',
     borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10
   }
 };
 
